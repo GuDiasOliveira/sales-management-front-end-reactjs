4a79309fb4427da7405b7abbe26043ca8c37dcfb
Gustavo Dias de Oliveira <gust.dias@gmail.com>
2018-10-30 08:34:56 -0300

    First test of viewing a seller, to implement see seller's sales.

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ class App extends Component {
       sellers: []
     };
     this.refresh();
+    this.viewSales = this.viewSales.bind(this);
   }
 
   refresh() {
@@ -43,12 +44,24 @@ class App extends Component {
       .catch(err => {console.log(err); alert('Failed to retrive the sellers');});
   }
 
+  viewSales(sellerId) {
+    (async () => {
+      const response = await fetch('http://localhost:8081/seller/' + sellerId);
+      const body = await response.json();
+      if (response.status !== 200) {
+        throw new Error(body.message);
+      }
+      return body;
+    })().then(res => JSON.stringify(res, null, 2))
+      .then(res => alert(res));
+  }
+
   render() {
     return (
       <div className="App">
         <main>
           <h1>Sellers:</h1>
-          <SellersView sellers={this.state.sellers} />
+          <SellersView sellers={this.state.sellers} onSeeSales={this.viewSales} />
         </main>
       </div>
     );
--- src/views/SellersView.js
+++ src/views/SellersView.js
@@ -14,7 +14,7 @@ export default class SellersView extends Component {
         </thead>
         <tbody>
           {this.props.sellers.map((seller, i) => (
-            <SellerRow key={seller.id} seller={seller} />
+            <SellerRow key={seller.id} seller={seller} onSeeSales={this.props.onSeeSales} />
           ))}
         </tbody>
       </table>
@@ -25,11 +25,23 @@ export default class SellersView extends Component {
 
 class SellerRow extends Component {
 
+  constructor(props) {
+    super(props);
+    this.handleClickSeeSales = this.handleClickSeeSales.bind(this);
+  }
+
+  handleClickSeeSales(e) {
+    this.props.onSeeSales(this.props.seller.id);
+  }
+
   render() {
     return(
       <tr>
         <td>{this.props.seller.id}</td>
         <td>{this.props.seller.name}</td>
+        <td>
+          <button onClick={this.handleClickSeeSales}>Report sales</button>
+        </td>
       </tr>
     );
   }
